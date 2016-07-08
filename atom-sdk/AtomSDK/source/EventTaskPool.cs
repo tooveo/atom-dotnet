277cef2c5ba795c0351018b03ff284178911ccc1
g8y3e <valentine.pavchuk@ironsrc.com>
2016-07-08 10:45:40 +0300

    wixed tabs

--- atom-sdk/AtomSDK/source/EventTaskPool.cs
+++ atom-sdk/AtomSDK/source/EventTaskPool.cs
@@ -7,66 +7,66 @@ using System.Diagnostics;
 
 namespace ironsource {
     class EventTaskPool {
-    	private ConcurrentQueue<Action> events_;
-    	private bool isRunning_;
+        private ConcurrentQueue<Action> events_;
+        private bool isRunning_;
 
-    	private List<Thread> workers_;
+        private List<Thread> workers_;
 
-    	private int maxEvents_;
+        private int maxEvents_;
 
-    	public EventTaskPool(int maxThreads, int maxEvents) {
-    		maxEvents_ = maxEvents;
-    		events_ = new ConcurrentQueue<Action>();
-    		isRunning_ = true;
+        public EventTaskPool(int maxThreads, int maxEvents) {
+            maxEvents_ = maxEvents;
+            events_ = new ConcurrentQueue<Action>();
+            isRunning_ = true;
 
-    		workers_ = new List<Thread>();
-    		ThreadStart threadMethodHolder = new ThreadStart(this.TaskWorker);
+            workers_ = new List<Thread>();
+            ThreadStart threadMethodHolder = new ThreadStart(this.TaskWorker);
 
-    		for (int index = 0; index < maxThreads; ++index) {
-    			Thread workerThread = new Thread(threadMethodHolder);
-    			workers_.Add(workerThread);
+            for (int index = 0; index < maxThreads; ++index) {
+                Thread workerThread = new Thread(threadMethodHolder);
+                workers_.Add(workerThread);
 
-    			workerThread.Start();
-    		}
-    	}
+                workerThread.Start();
+            }
+        }
 
         /// <summary>
         /// Stop this instance.
         /// </summary>
-    	public void Stop() {
-    		isRunning_ = false;
+        public void Stop() {
+            isRunning_ = false;
 
-    		foreach (Thread thread in workers_) {
-			    thread.Join();
-    		}
-    	}
+            foreach (Thread thread in workers_) {
+                thread.Join();
+            }
+        }
 
         /// <summary>
         /// Tasks the worker.
         /// </summary>
-    	private void TaskWorker() {
-    		while (isRunning_) {
-    			Action eventAction;
-    			if (!events_.TryDequeue(out eventAction)) {
-    				Thread.Sleep(25);
-    				continue;
-    			}
+        private void TaskWorker() {
+            while (isRunning_) {
+                Action eventAction;
+                if (!events_.TryDequeue(out eventAction)) {
+                    Thread.Sleep(25);
+                    continue;
+                }
 
-    			eventAction();
-    		}
-    	}
+                eventAction();
+            }
+        }
 
         /// <summary>
         /// Adds the event.
         /// </summary>
         /// <param name="eventAction">Event action.</param>
-    	public void addEvent(Action eventAction) {
-    		if (events_.Count > maxEvents_) {
+        public void addEvent(Action eventAction) {
+            if (events_.Count > maxEvents_) {
                 // fixme raise exception
-			    return;
-    		}
-    		events_.Enqueue(eventAction);
-    	}
+                return;
+            }
+            events_.Enqueue(eventAction);
+        }
     }
 }
 
