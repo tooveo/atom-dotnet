8a5b6fc0353adcd300d0e465c9a4571194381c54
g8y3e <valentine.pavchuk@ironsrc.com>
2016-07-12 20:30:06 +0300

    fixed travis and project

---
+++ atom-sdk/AtomSDKTests/ResponseTest.cs
@@ -0,0 +1,26 @@
+﻿
+using NUnit.Framework;
+
+namespace ironsource
+{
+    namespace test
+    {
+        [TestFixture ()]
+        public class Response_Tests
+        {
+            [Test ()]
+            public void ResponseProperties_Test ()
+            {
+                string expectedError = "test error";
+                string expectedData = "test data";
+                int expectedStatus = 200;
+
+                Response response = new Response (expectedError, expectedData, expectedStatus);
+
+                Assert.AreEqual (expectedError, response.error);
+                Assert.AreEqual (expectedData, response.data);
+                Assert.AreEqual (expectedStatus, response.status);
+            }
+        }
+    }
+}
