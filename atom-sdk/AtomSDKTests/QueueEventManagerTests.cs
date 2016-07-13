66276bdf85952352615c0afad8233531d17f4d32
g8y3e <valentine.pavchuk@ironsrc.com>
2016-07-13 11:50:45 +0300

    fixed project

---
+++ atom-sdk/AtomSDKTests/QueueEventManagerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ironsource {
+    namespace test {        
+        [TestFixture()]
+        public class QueueEventManager_Tests {
+            [Test()]
+            public void Constructor_Test() {  
+                IEventManager eventManager = new QueueEventManager();
+
+                Assert.AreEqual(eventManager.getEvent(""), null);
+            }           
+        }
+    }
+}
---
+++ atom-sdk/AtomSDKTests/ResponseTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ironsource {
+    namespace test {        
+        [TestFixture()]
+        public class Response_Tests {
+            [Test()]
+            public void ResponseProperties_Test() {
+                string expectedError = "test error";
+                string expectedData = "test data";
+                int expectedStatus = 200;
+
+                Response response = new Response(expectedError, expectedData, expectedStatus);
+
+                Assert.AreEqual(expectedError, response.error);
+                Assert.AreEqual(expectedData, response.data);
+                Assert.AreEqual(expectedStatus, response.status);
+            }           
+        }
+    }
+}
