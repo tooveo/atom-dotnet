c29685815da7bf48888a3595778dddcccf326e02
zeevmanilovich <zeev.manilovich@ironsrc.com>
2017-01-01 17:57:32 +0200

    changing doc generation script

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash -x
 doxygen doxyfile
 
 TARGET_BRANCH="gh-pages"
-mkdir $TARGET_BRANCH
-cd $TARGET_BRANCH
+mkdir ${TARGET_BRANCH}
+cd ${TARGET_BRANCH}
  
-git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-dotnet.git
+git clone -b ${TARGET_BRANCH} --single-branch https://github.com/ironSource/atom-dotnet.git
 
 cd atom-dotnet
 cp -r ../../doc/html/* .
@@ -13,8 +14,8 @@ git add .
 git commit -m "Deploy to GitHub Pages"
 
 # Now that we're all set up, we can push.
-git push origin $TARGET_BRANCH
+git push origin ${TARGET_BRANCH}
 
 cd ../../
-rm -r -f $TARGET_BRANCH
+rm -r -f ${TARGET_BRANCH}
 rm -r -f doc
